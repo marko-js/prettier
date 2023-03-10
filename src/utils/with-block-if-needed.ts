c27d7a0c8b45fc2cd1fa88741b87a92435ba9a54
LuLaValva <lukelavalva@gmail.com>
2023-03-09 18:06:03 -0800

    fix: brought back Dylan's stuff

--- src/utils/with-block-if-needed.ts
+++ src/utils/with-block-if-needed.ts
@@ -1,15 +1,32 @@
 import { types as t } from "@marko/compiler";
-import { doc, Doc } from "prettier";
+import { doc, Doc, ParserOptions } from "prettier";
+import { enclosedNodeTypeReg } from "../constants";
+import outerCodeMatches from "./outer-code-matches";
 
 const { builders: b } = doc;
 
-export default function withBlockIfNeeded(nodes: t.Statement[], docs: Doc[]) {
-  if (nodes.length > 1) {
+export default function withBlockIfNeeded(
+  nodes: t.Statement[],
+  opts: ParserOptions,
+  /* must use a factory function because `printDocToString` has side effects */
+  getDocs: () => Doc[]
+) {
+  if (
+    nodes.length > 1 ||
+    (!enclosedNodeTypeReg.test(nodes[0].type) &&
+      outerCodeMatches(
+        doc.printer.printDocToString(getDocs(), {
+          ...opts,
+          printWidth: 0,
+        }).formatted,
+        /[\n\r]/y
+      ))
+  ) {
     return [
-      b.indent([b.ifBreak(["{", b.line]), b.join(b.hardline, docs)]),
+      b.indent([b.ifBreak(["{", b.line]), b.join(b.hardline, getDocs())]),
       b.ifBreak([b.line, "}"]),
     ];
   }
 
-  return docs;
+  return getDocs();
 }
