f108c46b6c35cb3973f0cfafa138ad1b3bdfe19b
dpiercey <dpiercey@ebay.com>
2026-04-14 09:07:38 -0700

    chore: upgrade deps

--- rolldown.config.mjs
+++ rolldown.config.mjs
@@ -4,10 +4,6 @@ export default defineConfig({
   platform: "node",
   input: "src/index.ts",
   external: [/^[^./]/, import.meta.resolve("./package.json")],
-  treeshake: {
-    // https://github.com/rolldown/rolldown/issues/8299
-    moduleSideEffects: "no-external",
-  },
   output: [
     {
       sourcemap: false,
